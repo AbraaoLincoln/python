2211ba58960cabb6015a26001da940fa50703117
AbraaoLincoln <44592065+AbraaoLincoln@users.noreply.github.com>
2019-08-24 18:39:10 -0300

    Add files via upload

--- thread.py
+++ thread.py
@@ -4,13 +4,16 @@ import os
 import mmap
 import posix_ipc
 import sys
+import signal
 
+index = 0
 mutex = threading.Lock()
 result = []
 
 #Definindo a função func
 def func_soma(valor_matriz1, valor_matriz2, modoDeEscrita=None):
     global result
+    global index
     #Regiao critica
     if modoDeEscrita == None:
         mutex.acquire()
@@ -18,14 +21,16 @@ def func_soma(valor_matriz1, valor_matriz2, modoDeEscrita=None):
         mutex.release()
     else:
         valor = valor_matriz1 + valor_matriz2
-        #print("valor", valor)
+        print("valor", valor)
         mamoria_map = None
         memoria = posix_ipc.SharedMemory("shm")
         mamoria_map = mmap.mmap(memoria.fd, memoria.size)
         memoria.close_fd()
-        semaforo_fun = posix_ipc.Semaphore("semafa")
+        semaforo_fun = posix_ipc.Semaphore("semaf")
         #Regiao critica
         semaforo_fun.acquire()
+        #print("asda")
+        mamoria_map.seek(index * 4)
         mamoria_map.write(valor.to_bytes(4,byteorder='big'))
         semaforo_fun.release()
 
@@ -43,7 +48,7 @@ def func_multiplicacao(valores, modoDeEscrita=None):
 
 #Definindo a funcao unroll
 def unroll(args, func, method, results):
-
+    global index
     if method == "thred":
         funcoes = []
         valor_coluna_linha = []
@@ -83,44 +88,71 @@ def unroll(args, func, method, results):
     else:
         i = 0
         j = 0
-        pid_processo_pai = os.getpid()
-
+        tamanho_da_memoria = len( args[0][0] ) * len( args[1] ) * 4
+        #print(tamanho_da_memoria)
         memoria_mapeada = None
-
-        memoria_compartilhada = posix_ipc.SharedMemory("shm", flags = posix_ipc.O_CREAT, mode = 0o777, size = 100)
+        #Memoria compartilhada
+        memoria_compartilhada = posix_ipc.SharedMemory("shm", flags = posix_ipc.O_CREAT, mode = 0o777, size = tamanho_da_memoria)
         memoria_mapeada = mmap.mmap(memoria_compartilhada.fd, memoria_compartilhada.size)
         memoria_compartilhada.close_fd()
-        semaforo = posix_ipc.Semaphore("semafa", flags = posix_ipc.O_CREAT, mode = 0o777,  initial_value=1)
-        semaforo_end = posix_ipc.Semaphore("end", flags = posix_ipc.O_CREAT,mode = 0o777, initial_value = 0)
         memoria_mapeada.seek(0)
-        #func(args[0][i][j], args[1][i][j], "fork")
-        pid = os.fork()
-        print("pid: ", pid, "pid_pai: ", pid_processo_pai)
-        while pid == 0:
-            if i != ( len(args[0]) ) and j != ( len(args[0][0]) ):
-                func(args[0][i][j], args[1][i][j], "fork")
-                i += 1
-                j += 1
+        #Semaforos
+        semaforo = posix_ipc.Semaphore("semaf", flags = posix_ipc.O_CREAT, mode = 0o777,  initial_value=1)
+        semaforo_end = posix_ipc.Semaphore("end", flags = posix_ipc.O_CREAT,mode = 0o777, initial_value = 0)
+        
+        pid_processo_pai = os.getpid()
+        #pid = os.getpid()
+        processoPai = True
+        #print("pid_pai: ", pid_processo_pai)
+
+        while processoPai:
+            #and j != ( len(args[0][0]) )
+            if i != ( len(args[0]) ):
                 pid = os.fork()
-                print("pid: ", pid)
+
+                if pid == 0:
+                    func(args[0][i][j], args[1][i][j], "fork")
+                    processoPai = False
+                    os.kill(os.getpid(), signal.SIGKILL)
+                else:
+                    if j < ( len(args[0][0]) - 1 ):
+                        j += 1
+                    else:
+                        i += 1
+                        j = 0
+                    index += 1
+                    #print("i: ", i, "j: ", j)
             else:
-                time.sleep(2)
-                semaforo_end.release()
+                time.sleep(1)
+                #semaforo_end.release()
+                processoPai = False
                 break
+            # if i != ( len(args[0]) ) and j != ( len(args[0][0]) ):
+            #     func(args[0][i][j], args[1][i][j], "fork")
+            #     i += 1
+            #     j += 1
+            #     pid = os.fork()
+            #     print("pid: ", pid)
+            # else:
+            #     time.sleep(2)
+            #     semaforo_end.release()
+            #     break
 
         if pid_processo_pai == os.getpid():
-            semaforo_end.acquire()
+            #semaforo_end.acquire()
+            memoria_mapeada.seek(0)
             for i in range((len(args[0]) * len(args[0][0]))):
-                memoria_mapeada.seek(0)
                 valorFinal = memoria_mapeada.read(4)
                 valor = int.from_bytes(valorFinal, byteorder='big')
-                print(str(valor))
-            print("Fechando as coisas")
+                print(str(valor), " ", end='')
+                if ( i + 1 ) % len(args[0][0]) == 0:
+                    print()
+            #print("Fechando as coisas...")
             memoria_mapeada.close()
             posix_ipc.unlink_shared_memory("shm")
             semaforo.close()
             semaforo_end.close()
-            sys.exit(0)
+            #sys.exit(0)
 
 #Criando as matrizes
 matriz1 = [[1,2,3], [3,4,3], [4,4,4]]
